9f4ce374fe4886fcbdfa3bd2e5dc217c04d57c49
Geiger <Matt.Geiger@pega.com>
2019-03-15 19:03:42 -0500

    Adding to Blank-Page

--- src/app/layout/blank-page/blank-page.module.ts
+++ src/app/layout/blank-page/blank-page.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbCarouselModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { BlankPageRoutingModule } from './blank-page-routing.module';
 import { BlankPageComponent } from './blank-page.component';
+import {
+  TimelineComponent,
+  NotificationComponent,
+  ChatComponent
+} from './components';
 
+import { StatModule } from '../../shared';
 @NgModule({
-    imports: [CommonModule, BlankPageRoutingModule],
-    declarations: [BlankPageComponent]
+    imports: [
+
+      CommonModule,
+      NgbCarouselModule,
+      NgbAlertModule,
+      StatModule,
+      BlankPageRoutingModule],
+    declarations: [
+      BlankPageComponent,
+      TimelineComponent,
+      NotificationComponent,
+      ChatComponent
+    ]
 })
 export class BlankPageModule {}
