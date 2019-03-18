267c87a028d4be54abf60497744b7ca4e758d90c
Geiger <Matt.Geiger@pega.com>
2019-03-18 15:06:05 -0500

    Unified Task List working

--- src/app/layout/blank-page/blank-page.component.ts
+++ src/app/layout/blank-page/blank-page.component.ts
@@ -1,3 +1,4 @@
+import { UnifiedtasklistComponent } from './../../../../.history/src/app/layout/blank-page/components/unifiedtasklist/unifiedtasklist.component_20190318090940';
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 
@@ -10,5 +11,20 @@ import { routerTransition } from '../../router.animations';
 export class BlankPageComponent implements OnInit {
     constructor() {}
 
-    ngOnInit() {}
-}
+    // ngOnInit() {}
+    ngOnInit() {
+      // UnifiedtasklistComponent.get;
+
+      // this.subscription = this.rwlservice.getMessage().subscribe(message => {
+      //   this.message = message;
+
+        // if (this.message.unifiedtasklist === 'Work' || this.message.unifiedtasklist === 'unifiedtasklist') {
+        // this.getunifiedtasklist();
+
+        // } else {
+        // this.getWorkBaskets(this.message.unifiedtasklist);
+        // }
+      }
+
+    }
+
