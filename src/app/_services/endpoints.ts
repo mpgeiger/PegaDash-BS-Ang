f433128080fabf0b417f410ec5d63a74ce57c173
Geiger <Matt.Geiger@pega.com>
2019-04-16 14:11:06 -0500

    CORS - 4

--- src/app/_services/endpoints.ts
+++ src/app/_services/endpoints.ts
@@ -1,7 +1,7 @@
 export const endpoints = {
   // Change this URL if you want to point the React application at another Pega server .
   // BASEURL: "http://172.17.130.3:9080/prweb/api/v1",
-  BASEURL: 'https://end2endcrmcommercialbankingnew.pegatsdemo.com/prweb/api/v1',
+  BASEURL: 'http://end2endcrmcommercialbankingnew.pegatsdemo.com/prweb/api/v1',
   AUTH: '/authenticate',
   CASES: '/cases',
   CASETYPES: '/casetypes',
