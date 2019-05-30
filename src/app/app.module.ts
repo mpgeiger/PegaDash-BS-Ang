085818718703ad333d6e0128b5230261b15ab054
Geiger <Matt.Geiger@pega.com>
2019-05-30 09:39:48 -0500

    Tomcat   -- added /dashboard as base-href

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,17 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClientJsonpModule  } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-// import {  ReactiveFormsModule } from '@angular/forms';
-// import {  } from '@angular/forms';
 import { AppMaterialModule } from './app-material/app-material.module';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { LanguageTranslationModule } from './shared/modules/language-translation/language-translation.module';
-
 import { AuthGuard } from './shared';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+// MPG Angular added /dashboard for TOMCAT and base-href
+import {APP_BASE_HREF} from '@angular/common';
+
+
 // import { HomeAccountsPageModule } from './layout/pega/home-accounts-page/home-accounts-page.module';
 // import { HomePageModule } from './layout/pega/home-page/home-page.module';
 
@@ -43,7 +42,10 @@ import { AppRoutingModule } from './app-routing.module';
       // HttpClientModule,
       // HttpParams
     ],
-    providers: [AuthGuard],
+    providers: [
+      AuthGuard
+      // MPG Angular added /dashboard for TOMCAT and base-href
+    , {provide: APP_BASE_HREF, useValue: '/dashboard'}],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
