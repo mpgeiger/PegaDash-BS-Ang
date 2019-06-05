c2dcb3baf50b2cd9686e260b45c186af0a5dc40e
Geiger <Matt.Geiger@pega.com>
2019-06-05 09:33:44 -0500

    cleanup some stuff

--- src/app/layout/pega/customer-info/welcome/welcome.component.ts
+++ src/app/layout/pega/customer-info/welcome/welcome.component.ts
@@ -1,5 +1,5 @@
 // import { StatComponent } from './../../../../shared/modules/stat/stat.component';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, AfterViewInit, SimpleChanges } from '@angular/core';
 import { StatComponent } from './../../../../shared/modules/stat/stat.component';
 
 @Component({
@@ -7,7 +7,7 @@ import { StatComponent } from './../../../../shared/modules/stat/stat.component'
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent implements OnInit, AfterViewInit  {
 
   // totalCurrentLiabilities: number = Number(localStorage.getItem('totalCurrentLiabilities'));
   // totalAvgMonthLiabilities = Number(localStorage.getItem('totalAvgMonthLiabilities'));
@@ -16,6 +16,7 @@ export class WelcomeComponent implements OnInit {
 
 
   constructor() { }
+  componentName = 'customer-info/welcom.component';
   @Input('FullName') fullName: string;
   @Input('LastAccess') lastAccess: string;
   // @Input('LastAccess') lastAccess: string;
@@ -24,11 +25,33 @@ export class WelcomeComponent implements OnInit {
   @Input() totalAvgMonthAssets: number;
   @Input() totalAvgMonthLiabilities: number;
 
+ displayUserName = '';
 
 
 
   ngOnInit() {
     console.log(' welcome.component lastAccess-->' + this.lastAccess);
   }
+  ngAfterViewInit(): void {
+    this.displayUserName =  localStorage.getItem('displayUserName');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    for (const propName in changes) {
+      const change = changes[propName];
+      const curVal  = JSON.stringify(change.currentValue);
+      const prevVal = JSON.stringify(change.previousValue);
+      console.log(this.componentName + '   Prev Val-->' + prevVal + '____Current-->' + curVal);
+
+      if (curVal !== prevVal) {
+        console.log(this.componentName + '\t\t NOT same!');
+      }
+         }
+
+
+    // if (changes.currentValue) {
+    //     this.totalCurrentAssets = chang;
+    // }
+}
 
 }
