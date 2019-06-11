553dbb31397a108dc2513364b552244e406b4e53
Geiger <Matt.Geiger@pega.com>
2019-06-11 10:25:54 -0500

    fallback

--- src/assets/Swift/PegaHelperExtension.js
+++ src/assets/Swift/PegaHelperExtension.js
@@ -7,10 +7,11 @@ function preparePegaAParams(gadgetName) {
   */
   pegaAParamObj.AppName="WebChatbot";
 	pegaAParamObj.HelpConfigurationName="BNY Swift";
- 	// pegaAParamObj.ContactId="7103716305";
-   pegaAParamObj.ContactId="7103716326";
-  //  pegaAParamObj.AccountNumber="1029311";
-   pegaAParamObj.AccountNumber="1029329";
+ 	pegaAParamObj.ContactId="7103716305";
+    pegaAParamObj.AccountNumber="1029311";
+   //  pegaAParamObj.AccountNumber="1029311";
+  //  pegaAParamObj.ContactId="7103716326";
+  //  pegaAParamObj.AccountNumber="1029329";
 
 	 /* pegaAParamObj.AccountNumber="9998765431"; */
 	pegaAParamObj.username=getCookie("UserName");
@@ -45,10 +46,10 @@ function getCookie(cname) {
 
 
 /* Set cookies. To be overwritten locally */
-  // setCookie("ContactID", "7103716305", 30);
-  setCookie("ContactID", "7103716326", 30);
-  // setCookie("AccountNumber","1029311", 30);
+  setCookie("ContactID", "7103716305", 30);
+  setCookie("AccountNumber","1029311", 30);
+  // setCookie("ContactID", "7103716326", 30);
 //  setCookie("AccountNumber","9999999975", 30);
- setCookie("AccountNumber","1029329", 30);
+//  setCookie("AccountNumber","1029329", 30);
  /* setCookie("AccountNumber","9998765431", 30);*/
   setCookie("UserName","Shea Parker", 30);
