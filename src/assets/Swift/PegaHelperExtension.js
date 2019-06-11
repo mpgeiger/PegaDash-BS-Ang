9e2579a383cc5e29bf18c83e4110f7d9c300aa38
Geiger <Matt.Geiger@pega.com>
2019-06-11 09:03:30 -0500

    CHAT Tweak

--- src/assets/Swift/PegaHelperExtension.js
+++ src/assets/Swift/PegaHelperExtension.js
@@ -9,7 +9,8 @@ function preparePegaAParams(gadgetName) {
 	pegaAParamObj.HelpConfigurationName="BNY Swift";
 /*  	pegaAParamObj.ContactId="7103716305"; */
   	pegaAParamObj.ContactId="7103716326";
-	pegaAParamObj.AccountNumber="1029311";
+	/* pegaAParamObj.AccountNumber="1029311"; */
+	 pegaAParamObj.AccountNumber="9998765431";
 	pegaAParamObj.username=getCookie("UserName");
   	pegaAParamObj.Site="Swift";
 	pegaAParamObj.pzSkinName="OnlineHelp";
@@ -44,5 +45,6 @@ function getCookie(cname) {
 /* Set cookies. To be overwritten locally
   setCookie("ContactID", "7103716305", 30);  */
   setCookie("ContactID", "7103716326", 30);
-  setCookie("AccountNumber","9999999975", 30);
+  /* setCookie("AccountNumber","9999999975", 30); */
+  setCookie("AccountNumber","9998765431", 30);
   setCookie("UserName","Shea Parker", 30);
