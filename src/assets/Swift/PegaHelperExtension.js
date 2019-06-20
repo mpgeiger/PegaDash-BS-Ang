2e19a17d468a45f7856891f05f9f296df94169d5
Geiger <Matt.Geiger@pega.com>
2019-06-20 13:56:34 -0500

    CHAT Acct 1350514775

--- src/assets/Swift/PegaHelperExtension.js
+++ src/assets/Swift/PegaHelperExtension.js
@@ -16,7 +16,7 @@ function preparePegaAParams(gadgetName) {
   //  pegaAParamObj.ContactId="7103716326";
   //  pegaAParamObj.AccountNumber="1029329";
 
-	pegaAParamObj.AccountNumber="1029311";
+	pegaAParamObj.AccountNumber="1350514775";
 	pegaAParamObj.username=getCookie("UserName");
   	pegaAParamObj.Site="Swift";
 	pegaAParamObj.pzSkinName="OnlineHelp";
@@ -58,5 +58,5 @@ function getCookie(cname) {
 //  setCookie("AccountNumber","9999999975", 30);
 //  setCookie("AccountNumber","1029329", 30);
   // setCookie("AccountNumber","9998765431", 30);
-  setCookie("AccountNumber","1029311", 30);
+  setCookie("AccountNumber","1350514775", 30);
   setCookie("UserName","Shea Parker", 30);
